73ede1bc806eff44b9eca2587cdd832f4c58747c
ForAeons <w.xy020203@gmail.com>
2023-12-22 14:12:57 +0800

    add allow origin func

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -22,6 +22,21 @@ func SetupCors(app *fiber.App, cfg *config.Config) {
 	app.Use(cors.New(cors.Config{
 		AllowCredentials: true,
 		AllowOrigins:     cfg.FrontendURL,
+		AllowOriginsFunc: allowedOrigins,
 		AllowHeaders:     strings.Join(AllowHeaders, ", "),
 	}))
+}
+
+// Allow anythin from https://*lms-cambodia-dev.netlify.app/
+// e.g. https://deploy-preview-21--lms-cambodia-dev.netlify.app/
+func allowedOrigins(s string) bool {
+	if strings.HasSuffix(s, "lms-cambodia-dev.netlify.app") {
+		return true
+	}
+
+	if strings.HasSuffix(s, "wxiaoyun.com") {
+		return true
+	}
+
+	return false
 }
