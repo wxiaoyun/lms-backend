40215a84ebf91fbc073407f58989728854c65b91
ForAeons <w.xy020203@gmail.com>
2024-01-11 16:26:23 +0800

    add endpoint for create and delete bookcopy

--- internal/router/book.go
+++ internal/router/book.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	bookhandler "lms-backend/internal/handler/book"
+	bookcopyhandler "lms-backend/internal/handler/bookcopy"
 	bookmarkhandler "lms-backend/internal/handler/bookmark"
 	loanhandler "lms-backend/internal/handler/loan"
 	reservationhandler "lms-backend/internal/handler/reservation"
@@ -20,6 +21,7 @@ func BookRoutes(r fiber.Router) {
 		Route(r, "/loan", BookLoanRoutes)
 		Route(r, "/reservation", BookReservationRoutes)
 		Route(r, "/bookmark", BookBookmarkRoutes)
+		Route(r, "/bookcopy", BookBookcopyRoutes)
 	})
 
 	Route(r, "/autocomplete", func(r fiber.Router) {
@@ -37,4 +39,8 @@ func BookReservationRoutes(r fiber.Router) {
 
 func BookBookmarkRoutes(r fiber.Router) {
 	r.Post("/", bookmarkhandler.HandleCreate)
+}
+
+func BookBookcopyRoutes(r fiber.Router) {
+	r.Post("/", bookcopyhandler.HandleCreate)
 }
